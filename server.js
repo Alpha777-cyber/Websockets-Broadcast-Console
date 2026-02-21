28616945ef8be30ed33c3396109821a4f2692c55
MUNEZERO Alpha <alphamnzr@gmail.com>
2026-02-21 09:27:29 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import { WebSocketServer,WebSocket } from "ws";
+
+const wss = new WebSocketServer({port:8000});
+
+//0:connecting
+//1:Open
+//2.Close
+//3.Closed
+
+wss.on('connection',(socket,request)=>{
+    const ip = request.socket.remoteAddress;
+
+    socket.on('message',(rawData)=>{
+        const message = rawData.toString();
+        console.log({ rawData});
+
+        wss.clients.forEach((client)=>{
+            if(client.readyState == WebSocket.OPEN) client.send(`sever broadcast :${message}`); 
+        })
+    });
+
+    socket.on('error',(err)=>{
+        console.error(`Error: ${err.message}: ${ip}`);
+    });
+
+    socket.on('close',()=>{
+        console.log('Client disconnected');
+    });
+
+});
+
+console.log("Websocket server is live on ws://localhost:8000");
---
+++ sockets.js
@@ -0,0 +1,41 @@
+const statusEL = document.getElementById("status");
+const log = document.getElementById("log");
+const input = document.getElementById("message-input");
+        
+//1.initiate the handshake
+const socket = new WebSocket('ws://localhost:8000');
+
+const appendLog = (label,message)=>{
+    const entry =  `${new Date().toLocaleTimeString()} ${label} ${message} \n`
+      log.textContent = entry + log.textContent;
+    }
+
+socket.addEventListener('open',()=>{
+    statusEL.textContent = 'CONNECTED: ws://localhost:8000';
+    statusEL.className = 'status-on';
+    appendLog('[SYSTEM]','Tunnel Established.');
+})
+
+socket.addEventListener('close',()=>{
+    statusEL.textContent = 'DISCONNECTED';
+    statusEL.className = 'status-off'
+    appendLog('[SYSTEM]','Tunnel Collapsed');
+})
+
+socket.addEventListener('message',(e)=>{
+    appendLog('[RECIEVED]', e.data);
+})
+
+document.getElementById('message-form')
+ .addEventListener('submit',(e) =>{
+    e.preventDefault();
+
+    if(socket.readyState != WebSocket.OPEN){
+        appendLog('[ERROR','No active tunnel found.');
+    }
+
+    const msg = input.ariaValueMax.trim();
+    socket.send(msg);
+    appendLog('[SENT],msg')
+    input.value = "";
+ })
